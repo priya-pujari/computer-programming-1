44970f5b53a1f54d97137e66fdfb8e38b73c50d7
priya-pujari <doripiku4@gmail.com>
2026-09-06 16:18:05 +0530

    Create q3_sum_and_mean_n_values.c

---
+++ q3_sum_and_mean_n_values.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n
+float num, sum=0,mean;
+printf ("enter the number of values (n): ");
+scanf ("%d",&n);
+printf (enter %d numbers :\n",n);
+for (int i=0; i<n; i++)
+{
+scanf ("%f",&num);
+sum+=num;
+}
+mean=sum/n;
+printf ("sum= %.2f\n mean=%.2f\n",sum,mean);
+}
